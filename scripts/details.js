e025cd587b8ec4cda275b95c704253a996e99755
Chiarasig <chiarasignori37@gmail.com>
2022-10-19 00:49:46 -0300

    nuevosCambios

---
+++ scripts/details.js
@@ -0,0 +1,31 @@
+let eventId=new URL(window.location.href).searchParams.get('id') //query param, para tomar del lado del detail, se toma la url 
+//window location y lo convertimos a una url para que sea interpretada por js y con "searchParam" obtiene todo los parametros
+//que traiga despues del signo de interrogación (son parametros)
+let detailView = document.getElementById('viewDetail')
+
+let eventDetail = data.events.filter(object =>
+    object._id == eventId
+)
+
+let evento = eventDetail[0]
+detailView.innerHTML =
+`
+<article class="d-flex flex-column container-fluid mt-5 mb-5 bg-indigo shadow p-3 bg-indigo p-2 text-dark bg-opacity-10" style="width: 35%;">
+<h5 class="text-center text-warning bg-dark fst-italic">${evento.name}</h5>
+<img
+  src= "${evento.image}",
+  alt=${evento.name}
+/>
+<p class="text-center pt-3 fst-italic">${evento.description}</p>
+<div class="d-flex justify-content-evenly flex-wrap font-monospace">
+  <p>Category: ${evento.category}</p>
+  <p>Date: ${evento.date}</p>
+  <p>Price: ${evento.price}</p>
+</div>
+<div class="d-flex justify-content-evenly flex-wrap font-monospace">
+  <p>Capacity: ${evento.capacity}</p>
+  <p>Assistance: ${evento.assistance}</p>
+  <p>Place: ${evento.place}</p>
+</div>
+</article>
+          `;
--- scripts/pastevents.js
+++ scripts/pastevents.js
@@ -11,52 +11,75 @@ function printCard (array, container){
               alt=${evento.name}
               height="60%"
             />
-            <h5 class="card-title text-center fst-italic">${evento.name}</h5>
+            <h5 class="card-title text-center">${evento.name}</h5>
             <p class="card-text text-center">${evento.description}</p>
             <div class="d-flex justify-content-evenly">
-              <p class="fst-italic">Precio US$ ${evento.price}</p>
-              <a href="../pages/details.html?id=${evento._id}" class="btn btn-danger">Details</a>
+              <p>Precio US$ ${evento.price}</p>
+              <a href="../pages/details.html?id=${evento._id}" class="btn btn-primary">Go somewhere</a>
             </div>
           </article>
-    `
+          `
+    
   })
 }
 printCard (data.events, containerCards)
 
-let search = document.getElementById("inputSearch");
-search.addEventListener("keyup", cambiosDelEvento => {
-  let inputUser = cambiosDelEvento.target.value
-  let filtro = data.events.filter((objetoEvento) =>
-  objetoEvento.name.toLowerCase().includes(inputUser.toLowerCase()));
-  containerCards.innerHTML ="";
-  printCard(filtro, containerCards); 
-});
+//codigo checkbox
+
 let categorias = document.getElementById("boxes");
 
 let checkbox = new Set(data.events.map((evento) => evento.category));
 
-checkbox = [...checkbox]
+checkbox = [...checkbox];
 
 checkbox.forEach((nombreCategoria) => {
-  categorias.innerHTML += 
-   ` <div class="form-check">
+  categorias.innerHTML += `
+    <div class="form-check">
         <input class="form-check-input" id="${nombreCategoria}" type="checkbox">
         <label class="form-check-label">${nombreCategoria}</label>
-    </div>`
-    ;
-    console.log(checkbox)
+    </div>
+    `;
+  console.log(checkbox);
 });
-let listCheck = []
-categorias.addEventListener("click", e=>{
-  if(e.target.checked){
-    listCheck = listCheck.concat(data.events.filter(evento=> evento.category.toLowerCase().includes(e.target.id.toLowerCase())))
-    console.log(listCheck);
-    containerCards.innerHTML = ''
-    printCard(listCheck,containerCards )}
-  else if(!e.target.checked){
-    console.log('else if')
-      listCheck = listCheck.filter(evento => !evento.category.toLowerCase().includes( e.target.id.toLowerCase() ) )
-      containerCards.innerHTML = ''
-      printCard(listCheck, containerCards)}
-  if (listCheck.length === 0){
-      printCard(data.events,containerCards)}})
+
+let listCheck = [];
+
+categorias.addEventListener(`click`, (e) => {
+  if (e.target.checked) {
+    listCheck = listCheck.concat(
+      data.events.filter((evento) =>
+        evento.category.toLowerCase().includes(e.target.id.toLowerCase())
+      )
+    );
+    containerCards.innerHTML = "";
+    printCard(listCheck, containerCards);
+  } else if (!e.target.checked) {
+    listCheck = listCheck.filter(
+      (evento) =>
+        !evento.category.toLowerCase().includes(e.target.id.toLowerCase())
+    );
+    containerCards.innerHTML = "";
+    printCard(listCheck, containerCards);
+  }
+  if (listCheck.length === 0) {
+    printCard(data.events, containerCards);
+  }
+});
+
+//entrada texto input
+
+let search = document.getElementById("inputSearch");
+search.addEventListener("keyup", (cambiosDelEvento) => {
+  let inputUser = cambiosDelEvento.target.value;
+  let filtro = [];
+  if (listCheck.length !== 0) {
+    filtro = listCheck.filter((objetoEvento) =>
+      objetoEvento.name.toLowerCase().includes(inputUser.toLowerCase())
+    );
+  }else{
+    filtro = data.events.filter((objetoEvento) =>
+    objetoEvento.name.toLowerCase().includes(inputUser.toLowerCase())
+  )}
+  containerCards.innerHTML = "";
+  printCard(filtro, containerCards);
+});
